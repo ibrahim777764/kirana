913a8275372b003cba212ec375bf173e3f167386
ibrahim777764 <ibrahimnaseer896@gmail.com>
2023-01-27 11:52:00 +0500

    add prodcts data

---
+++ db/migrate/20230127062449_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :product_name
+      t.text :product_description
+      t.integer :product_price
+      t.timestamps
+    end
+  end
+end
